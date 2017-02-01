9ad0058905910f62eb686207cdd096209c927403
Benedikt Werner <1benediktwerner@gmail.com>
2017-02-01 20:29:11 +0100

    New website

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,14 @@
 $(function() {
-  
+  navbarScroll();
 });
+
+function navbarScroll() {
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 10) {
+      $("nav.navbar").removeClass("top");
+    }
+    else {
+      $("nav.navbar").addClass("top");
+    }
+  });
+}
