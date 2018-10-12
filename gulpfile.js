425bf3fd0c1768d62a52050481ffa275ea236e3f
Benedikt Werner <1benediktwerner@gmail.com>
2018-10-12 15:52:01 +0200

    Gulp: Fix browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -6,42 +6,44 @@ var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-function bootstrap() {
-    return gulp.src([
-        './node_modules/bootstrap/dist/**/*',
-        '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-        '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-        .pipe(gulp.dest('./vendor/bootstrap'));
-}
-
-function fontawesome() {
-    return gulp.src([
-        './node_modules/@fortawesome/**/*'
-    ])
-        .pipe(gulp.dest('./vendor'));
-}
-
-function jQuery() {
-    return gulp.src([
-        './node_modules/jquery/dist/*',
-        '!./node_modules/jquery/dist/core.js'
-    ])
-        .pipe(gulp.dest('./vendor/jquery'));
-}
-
-function jQuery_easing() {
-    return gulp.src([
-        './node_modules/jquery.easing/*.js'
-    ])
-        .pipe(gulp.dest('./vendor/jquery-easing'));
-}
-
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', gulp.parallel(bootstrap, fontawesome, jQuery, jQuery_easing));
+const vendor = gulp.parallel(
+    // Bootstrap
+    function vendor_bootstrap() {
+        return gulp.src([
+            './node_modules/bootstrap/dist/**/*',
+            '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+            '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+        ])
+            .pipe(gulp.dest('./vendor/bootstrap'));
+    },
+    // Font Awesome
+    function vendor_fontawesome() {
+        return gulp.src([
+            './node_modules/@fortawesome/**/*'
+        ])
+            .pipe(gulp.dest('./vendor'));
+    },
+    // jQuery
+    function vendor_jQuery() {
+        return gulp.src([
+            './node_modules/jquery/dist/*',
+            '!./node_modules/jquery/dist/core.js'
+        ])
+            .pipe(gulp.dest('./vendor/jquery'));
+    },
+    // jQuery Easing
+    function vendor_jQuery_easing() {
+        return gulp.src([
+            './node_modules/jquery.easing/*.js'
+        ])
+            .pipe(gulp.dest('./vendor/jquery-easing'));
+    }
+);
+gulp.task("vendor", vendor);
 
 // Compile SCSS
-gulp.task('css:compile', function () {
+function css_compile() {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass.sync({
             outputStyle: 'expanded'
@@ -51,10 +53,10 @@ gulp.task('css:compile', function () {
             cascade: false
         }))
         .pipe(gulp.dest('./css'))
-});
+}
 
 // Minify CSS
-gulp.task('css:minify', function () {
+function css_minify() {
     return gulp.src([
         './css/*.css',
         '!./css/*.min.css'
@@ -65,13 +67,14 @@ gulp.task('css:minify', function () {
         }))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
-});
+}
 
 // CSS
-gulp.task('css', gulp.series('css:compile', 'css:minify'));
+const css = gulp.series(css_compile, css_minify);
+gulp.task('css', css);
 
 // Minify JavaScript
-gulp.task('js', function () {
+function js() {
     return gulp.src([
         './js/*.js',
         '!./js/*.min.js'
@@ -82,26 +85,34 @@ gulp.task('js', function () {
         }))
         .pipe(gulp.dest('./js'))
         .pipe(browserSync.stream());
-});
+}
+gulp.task("js", js);
 
-// Default task
-gulp.task('default', gulp.parallel('css', 'js', 'vendor'));
-
-// Configure the browserSync task
-gulp.task('browserSync', function () {
+// Browser Sync
+function serve(done) {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-});
+    done();
+};
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+// Default task
+gulp.task('default', gulp.parallel(css, js, vendor));
 
 // Dev task
 gulp.task('dev', gulp.series(
-    gulp.parallel('css', 'js', 'browserSync'),
-    function () {
-        gulp.watch('./scss/*.scss', ['css']);
-        gulp.watch('./js/*.js', ['js']);
-        gulp.watch('./*.html', browserSync.reload);
-    })
-);
+    gulp.parallel(css, js),
+    serve,
+    function watch() {
+        gulp.watch('./scss/*.scss', css);
+        gulp.watch('./js/*.js', js);
+        gulp.watch('./*.html', reload);
+    }
+));
